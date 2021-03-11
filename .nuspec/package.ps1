5547a58cb407e44d8dc4c044e4d6537fe62b8637
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-03-11 16:04:30 -0600

    [build] bootstrap a local .\bin\dotnet\ with .NET Workloads
    Based on: https://github.com/jonathanpeppers/maui-workload
    
    This adds a new `DotNet.csproj` that provisions a local .NET 6 install
    into `.\bin\dotnet\`.
    
    Next, it uses versions defined in `eng/Versions.props` as required by
    .NET version bumping infrastructure (called Darc):
    
        <Project>
          <PropertyGroup>
            <MicrosoftNETSdkPackageVersion>6.0.100-preview.2.21155.3</MicrosoftNETSdkPackageVersion>
            <MicrosoftAndroidSdkPackageVersion>11.0.200-ci.main.148</MicrosoftAndroidSdkPackageVersion>
            <MicrosoftMacCatalystSdkPackageVersion>14.3.100-ci.main.337</MicrosoftMacCatalystSdkPackageVersion>
            <MicrosoftiOSSdkPackageVersion>14.4.100-ci.main.1192</MicrosoftiOSSdkPackageVersion>
          </PropertyGroup>
        </Project>
    
    Next, we can use these versions to consume NuGet packages for workloads:
    
        <PackageDownload Include="Microsoft.NET.Workload.Android"     Version="[$(MicrosoftAndroidSdkPackageVersion)]" />
        <PackageDownload Include="Microsoft.NET.Workload.MacCatalyst" Version="[$(MicrosoftMacCatalystSdkPackageVersion)]" />
        <PackageDownload Include="Microsoft.NET.Workload.iOS"         Version="[$(MicrosoftiOSSdkPackageVersion)]" />
    
    Then the other packs they depend on:
    
        <PackageDownload Include="Microsoft.Android.Ref"            Version="[$(MicrosoftAndroidSdkPackageVersion)]" />
        <PackageDownload Include="Microsoft.Android.Sdk.win-x64"    Version="[$(MicrosoftAndroidSdkPackageVersion)]" Condition="$([MSBuild]::IsOSPlatform('windows'))" />
        <PackageDownload Include="Microsoft.Android.Sdk.osx-x64"    Version="[$(MicrosoftAndroidSdkPackageVersion)]" Condition="$([MSBuild]::IsOSPlatform('osx'))" />
        <PackageDownload Include="Microsoft.Android.Sdk.BundleTool" Version="[$(MicrosoftAndroidSdkPackageVersion)]" />
        <PackageDownload Include="Microsoft.MacCatalyst.Ref"        Version="[$(MicrosoftMacCatalystSdkPackageVersion)]" />
        <PackageDownload Include="Microsoft.MacCatalyst.Sdk"        Version="[$(MicrosoftMacCatalystSdkPackageVersion)]" />
        <PackageDownload Include="Microsoft.iOS.Ref"                Version="[$(MicrosoftiOSSdkPackageVersion)]" />
        <PackageDownload Include="Microsoft.iOS.Sdk"                Version="[$(MicrosoftiOSSdkPackageVersion)]" />
    
    After doing this, I can build .NET 6 projects with:
    
        > .\bin\dotnet\dotnet.exe build .\src\Controls\samples\Controls.Sample.SingleProject\Maui.Controls.Sample.SingleProject.csproj
    
    I can even build MacCatalyst apps on Windows!
    
    ~~ Other Changes ~~
    
    * Rework CI setup to use .\bin\dotnet\dotnet
    * We don't need boots or URLs anymore
    * Fixed `MSBuildTests` to use .\bin\dotnet\dotnet if found and fall
      back to the system `dotnet`
    
    ~~ TODO ~~
    
    This obviously will change the Maui team's workflow, so:
    
    * We need a script (or something) to launch VS Windows configured to
      use the `.\bin\dotnet\dotnet.exe`, similar to:
      https://github.com/jonathanpeppers/maui-workload/blob/main/scripts/dogfood.ps1
    * Need VS Mac support
    * Need docs explaining how to do local dev, etc.
    
    ~~ What problems does this solve? ~~
    
    * MacCatalyst builds on Windows! (offline build that checks C#)
    * Building Maui always gets you the right things installed.
    * Maui becoming a .NET workload will be a breeze. We can copy files
      into `.\bin\dotnet\sdk-manifests` and `.\bin\dotnet\packs`.
    * We can use Darc to bump dependencies within .NET:
    
    https://github.com/dotnet/arcade/blob/main/Documentation/Darc.md

--- .nuspec/package.ps1
+++ .nuspec/package.ps1
@@ -1,6 +1,17 @@
-param ($configuration)
+param ([string] $configuration = 'Debug')
 
-dotnet pack $PSScriptRoot\..\Microsoft.Maui-net6.sln `
+$ext = if ($IsWindows) { ".exe" } else { "" }
+$dotnet = Join-Path $PSScriptRoot ../bin/dotnet/dotnet$ext
+$sln = Join-Path $PSScriptRoot ../Microsoft.Maui-net6.sln
+$artifacts = Join-Path $PSScriptRoot ../artifacts
+
+if (-not (Test-Path $dotnet))
+{
+    $csproj = Join-Path $PSScriptRoot ../src/DotNet/DotNet.csproj
+    & dotnet build $csproj -bl:$artifacts/dotnet.binlog
+}
+
+& $dotnet pack $sln `
     -c:$configuration `
     -p:SymbolPackageFormat=snupkg `
-    -bl:$PSScriptRoot/../artifacts/maui.binlog
+    -bl:$artifacts/maui.binlog
